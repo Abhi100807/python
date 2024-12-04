b2db24324dbda6abdef08ba14cdbb687a0965ca8
abhiram <abhi998937@gmail.com>
2024-12-04 16:11:18 +0530

    index position in list

---
+++ 4-12-2024-index-list.py
@@ -0,0 +1,5 @@
+x = eval(input("enter the fruit list of min 5 fruits: "))
+second_fruit = x[1]
+fourth_fruit = x[3]
+print(f"The second fruit is: {second_fruit}")
+print(f"the fourth fruit is: {fourth_fruit}")
