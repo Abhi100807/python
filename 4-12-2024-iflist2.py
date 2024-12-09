371090039e005313d5583eac8d59f526be7e4715
abhiram <abhi998937@gmail.com>
2024-12-09 16:22:26 +0530

    index in tuple

---
+++ 4-12-2024-iflist2.py
@@ -0,0 +1,3 @@
+x =eval(input("enter the list: "))
+if "sara" and "jhon" in x:
+    print(f"the sara position is: {x["sara"]}")
---
+++ 9-12-2024-tuple-index.py
@@ -0,0 +1,3 @@
+x = eval(input("enter the tuple: "))
+index = int(input("enter the value: "))
+print(x[index])
